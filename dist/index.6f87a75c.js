184a97d4fed1752ed94e7775e2f0daff50ec3de9
David Cantós López <dcantos@uoc.edu>
2023-11-07 09:58:33 +0100

    Edit Route of header.html

---
+++ dist/index.6f87a75c.js
@@ -0,0 +1,3 @@
+console.log("Hello world");//# sourceMappingURL=index.6f87a75c.js.map
+
+//# sourceMappingURL=index.6f87a75c.js.map
---
+++ dist/index.e63c0b93.js
@@ -0,0 +1,3 @@
+console.log("Hello world");//# sourceMappingURL=index.e63c0b93.js.map
+
+//# sourceMappingURL=index.e63c0b93.js.map
